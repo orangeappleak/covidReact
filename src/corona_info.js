9460f658ba0d052cdcb4d65fd6fed0a7f2907cea
orangeappleak <appannagarikarthik.123@gmail.com>
2021-01-06 16:55:52 +0530

    testing div transitions using react-spring

--- src/corona_info.js
+++ src/corona_info.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useTrail,useTransition} from 'react-spring';
+import {useTransition,animated} from 'react-spring';
 
 
 const page_style = [
@@ -11,26 +11,43 @@ const page_style = [
 
 function Transition({children}){
     let items = React.Children.toArray(children);
-    let trans = useTransition()
+    console.log(items);
+    let trans = useTransition(items.length,{
+        from: {opacity: 0},
+        enter: {opacity: 1},
+        exit : {opacity: 0}
+    })
+
+    //TODO: transitions are not getting injeected into the final rendered html,find a way to inject
+    //the transitions.
+    return(
+        <div>
+            {trans.map((el,index) => (
+                <animated.div key={index}>
+                <h1>{console.log(el)}</h1>
+                </animated.div>
+            ))}
+        </div>
+    )
 }
 
 
 function VirusInfo(){
     return(
-        <div id="Virus-Info">
-           <div className="pages" id="page1" style ={page_style[0]}>
-                <h1>This is page1</h1>
-           </div>
+        <Transition>
+                <div className="pages" id="page1" style ={page_style[0]}>
+                    <h1>This is page1</h1>
+                </div>
 
-           <div className="pages" id="page2" style ={page_style[1]}>
-                <h1>This is page2</h1>
-           </div>
+                <div className="pages" id="page2" style ={page_style[1]}>
+                    <h1>This is page2</h1>
+                </div>
 
-           <div className="pages" id="page3" style ={page_style[2]}>
-                <h1>This is page3</h1>
-           </div>
-
-        </div>
+                <div className="pages" id="page3" style ={page_style[2]}>
+                    <h1>This is page3</h1>
+                </div>
+        </Transition>
+        
     )
 }
 
